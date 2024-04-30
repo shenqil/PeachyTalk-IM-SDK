ff3732d22f2f6c53cc1f70f9cf4bf4fb274ed654
shenqi.lv <248120694@qq.com>
2024-04-30 21:16:07 +0800

    feat: 完成协议层发送消息

--- lib/protocolLayer/mqtt/index.ts
+++ lib/protocolLayer/mqtt/index.ts
@@ -2,15 +2,94 @@
  * @Author: shenqi.lv 248120694@qq.com
  * @Date: 2024-04-28 18:42:23
  * @LastEditors: shenqi.lv 248120694@qq.com
- * @LastEditTime: 2024-04-29 10:29:51
+ * @LastEditTime: 2024-04-30 21:13:36
  * @FilePath: \PeachyTalk-IM-SDK\lib\protocolLayer\mqtt\mqtt.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import mqtt, { MqttClient } from "mqtt"
 
+const msgTopicPrefix = 'MSG'
+
 interface ILoginInfo {
     username: string,
     password: string
+}
+
+
+/**
+ * 消息类型
+ */
+export enum IMsgTypeEnum {
+    /**
+     * 文本
+     */
+    TEXT = 0,
+    /**
+     * 图片
+     */
+    IMAGE = 1,
+    /**
+     * 语音
+     */
+    AUDIO = 2,
+    /**
+     * 视频
+     */
+    VEDIO = 3,
+    /**
+     * 视频
+     */
+    CUSTOM = 4,
+}
+
+/**
+ * 聊天类型
+ */
+export enum IChatType {
+    /**
+     * 单聊消息
+     */
+    CHAT = 0,
+    /**
+     * 群聊消息
+     */
+    GROUP_CHAT = 1,
+    /**
+     * 系统消息
+     */
+    SYSTEM = 2,
+}
+
+/**
+ * 消息体
+ */
+export interface IMsg {
+    from: string; // 发送者
+    name: string; // 发送者昵称
+    avatarUrl: string; // 发送者头像
+
+    to: string; // 接收者
+    toName: string; // 接收人的昵称
+    toAvatarUrl: string; // 接收人的头像
+
+    chatType: IChatType; // 聊天类型 单聊、群聊
+    msgType: IMsgTypeEnum; // 消息类型
+
+    id: string; // 消息id
+    payload: string; // 消息内容
+
+    at: string; // 会话是否有@我的标识符
+
+    replyMsgId: string; // 回复消息ID
+    replyContent: string; // 回复内容
+
+    clientTime: string; // 客户端时间
+    serverTime: bigint; // 服务端时间
+
+    cMsgId: bigint; // 客户端唯一消息id
+    msgStatus: number; // 业务状态
+
+    extends: string[]; // 扩展字段
 }
 
 class ChatProtocol {
@@ -50,7 +129,8 @@ class ChatProtocol {
             // 发起一次连接
             this.client = mqtt.connect("websockets://localhost:9001", {
                 username: loginInfo.username,
-                password: loginInfo.password
+                password: loginInfo.password,
+                clean: false, // 保持会话
             });
 
             // 监听连接成功事件
@@ -79,6 +159,20 @@ class ChatProtocol {
      */
     logout() {
         this.destoryClient()
+    }
+
+    /**
+     * 发送消息
+     * @param msg 
+     * @returns 
+     */
+    async sendMsg(msg: IMsg) {
+        if (!this.client) {
+            throw new Error("未登陆")
+        }
+
+        const topic = `${msgTopicPrefix}/${msg.chatType == IChatType.GROUP_CHAT ? "GROUP" : ""}${msg.to}/${msg.from}`
+        return await this.client.publishAsync(topic, JSON.stringify(msg), { qos: 0 })
     }
 }
 
