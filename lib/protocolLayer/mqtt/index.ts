040e5c2616e453ae9530f9304ee6dd03024e9d1d
shenqi.lv <248120694@qq.com>
2024-05-09 20:52:56 +0800

    feat: mqtt协议层抛出连接中，已连接，已断开，接受到消息

--- lib/protocolLayer/mqtt/index.ts
+++ lib/protocolLayer/mqtt/index.ts
@@ -2,14 +2,69 @@
  * @Author: shenqi.lv 248120694@qq.com
  * @Date: 2024-04-28 18:42:23
  * @LastEditors: shenqi.lv 248120694@qq.com
- * @LastEditTime: 2024-05-08 21:13:21
+ * @LastEditTime: 2024-05-09 20:49:14
  * @FilePath: \PeachyTalk-IM-SDK\lib\protocolLayer\mqtt\mqtt.ts
  * @Description: 对MQTT进行封装
  */
 import mqtt, { MqttClient } from "mqtt"
+import log from "@/utils/log"
+import EventBus from "@/utils/eventBus"
 
 const msgTopicPrefix = 'MSG'
 const clientId = 'DESKTOP'
+
+
+
+/**
+ * 断开类型
+ */
+export enum EDisconnectType {
+    UNKNOWN, // 未知原因断开
+    NORMAL, // 正常断开
+    HEARTBEAT_TIMEOUT, // 心跳超时
+    WS_ERROR,
+    WS_CLOSE,
+}
+
+/**
+ * 连接层所有的事件名称
+ * */
+export const ETransportLayerEventName = Object.freeze({
+    /**
+     * @desc 连接中
+     * */
+    CONNECTING: 'CONNECTING',
+    /**
+     * @desc 已连接
+     * */
+    CONNECTED: 'CONNECTED',
+    /**
+     * @desc 断开连接
+     * */
+    DISCONNECTED: 'DISCONNECTED',
+    /**
+     * @desc 接收到消息
+     * */
+    MESSAGE_RECEIVED: 'MESSAGE_RECEIVED'
+})
+
+/**
+ * @desc 连接层所有的事件
+ * */
+export interface ITransportLayerEvent {
+    [ETransportLayerEventName.CONNECTING]: () => void;
+    [ETransportLayerEventName.CONNECTED]: () => void;
+    [ETransportLayerEventName.DISCONNECTED]: (type: EDisconnectType, reason?: any) => void;
+    [ETransportLayerEventName.MESSAGE_RECEIVED]: (topic: string, data: Uint8Array) => void;
+};
+
+/**
+ * 当前连接状态
+ */
+export type IConnectStatus =
+    'CONNECTING'
+    | "CONNECTED"
+    | "DISCONNECTED"
 
 interface ILoginInfo {
     username: string,
@@ -80,40 +135,73 @@ export interface IMQTTMsg {
 
 class ChatProtocol {
     // 唯一连接实例
-    private client: MqttClient | null
+    #client: MqttClient | null
+    // 事件
+    #eventBus: EventBus<ITransportLayerEvent> = new EventBus<ITransportLayerEvent>();
+    // 最后一条消息
+    #lastDisconnectReason: {
+        type: EDisconnectType,
+        reason?: any
+    }
+    // 连接状态
+    #connectStatus: IConnectStatus = "DISCONNECTED"
+    get connectStatus() {
+        return this.#connectStatus
+    }
+    set connectStatus(v: IConnectStatus) {
+        if (this.#connectStatus !== v) {
+            this.#connectStatus = v
+            switch (v) {
+                case "CONNECTED":
+                    this.#eventBus.emit(ETransportLayerEventName.CONNECTED)
+                    break;
+                case "CONNECTING":
+                    this.#eventBus.emit(ETransportLayerEventName.CONNECTING)
+                    break;
+                case "DISCONNECTED":
+                    this.#eventBus.emit(ETransportLayerEventName.DISCONNECTED, this.#lastDisconnectReason.type, this.#lastDisconnectReason.reason)
+                    break;
+                default:
+                    log.error(`[mqtt][connectStatus] 设置了未知状态 connectStatus=${v}`)
+                    break;
+            }
+        }
+    }
+
     constructor() {
-        this.client = null
+        this.#client = null
+        this.#lastDisconnectReason = {
+            type: EDisconnectType.UNKNOWN,
+            reason: undefined
+        }
     }
 
     /**
      * 销毁实例
      */
     private destoryClient() {
-        if (this.client) {
-            console.log('[im][mqtt][destoryClient]');
+        if (this.#client) {
+            log.debug('[mqtt][destoryClient]');
 
-            this.client.removeAllListeners()
+            this.#client.removeAllListeners()
 
-            this.client.end(false, () => {
-                console.log('[im][mqtt][destoryClient] Closed');
+            this.#client.end(false, () => {
+                log.info('[mqtt][destoryClient] Closed');
             })
-            this.client = null
+            this.#client = null
         }
     }
 
 
-    /**
-     * 登录
-     * @param loginInfo 
-     * @returns 
-     */
+
     private _login(loginInfo: ILoginInfo) {
         return new Promise((resolve, reject) => {
+            log.info('[mqtt][login]');
 
             this.destoryClient()
 
             // 发起一次连接
-            this.client = mqtt.connect("websockets://localhost:9001", {
+            this.#client = mqtt.connect("websockets://localhost:9001", {
                 username: loginInfo.username,
                 password: loginInfo.password,
                 clientId: `${loginInfo.username}-${clientId}`,
@@ -121,38 +209,69 @@ class ChatProtocol {
             });
 
             // 接受消息
-            this.client.on("message", this.onMessage.bind(this))
+            this.#client.on("message", this.onMessage.bind(this))
 
             // 监听连接成功事件
-            this.client.on('connect', function () {
-                console.log('[im][mqtt][login] Connected to MQTT server');
+            this.#client.on('connect', () => {
+                log.info('[mqtt][login] Connected to MQTT server');
                 resolve(true)
             });
 
             // 监听连接错误事件
-            this.client.on('error', function (error) {
-                console.error('[im][mqtt][login] Connection to MQTT server failed:', error);
+            this.#client.on('error', (error) => {
+                this.#lastDisconnectReason = {
+                    type: EDisconnectType.WS_ERROR,
+                    reason: error
+                }
+                log.error('[mqtt][login] Connection to MQTT server failed:', error);
                 reject(error)
             });
 
             // 监听断开连接事件
-            this.client.on('close', function () {
-                console.log('[im][mqtt][login] Disconnected from MQTT server');
+            this.#client.on('close', () => {
+                this.#lastDisconnectReason = {
+                    type: EDisconnectType.WS_CLOSE
+                }
+                log.info('[mqtt][login] Disconnected from MQTT server');
                 reject("onclose")
             });
 
         })
     }
+    /**
+     * 登录
+     * @param loginInfo 
+     * @returns 
+     */
     async login(loginInfo: ILoginInfo) {
-        const res = await this._login(loginInfo)
-        await this.subscribeMsg(loginInfo.username)
-        return res
+
+        this.connectStatus = "CONNECTING"
+        return this._login(loginInfo)
+            .then(() => {
+                return this.subscribeMsg(loginInfo.username)
+            })
+            .then(() => {
+                this.connectStatus = "CONNECTED"
+            })
+            .catch(err => {
+                this.connectStatus = "DISCONNECTED"
+                throw err
+            })
+
+        // await this.#client?.subscribeAsync('$SYS/broker/subscriptions/#')
+        // await this.#client?.subscribeAsync('$SYS/#')
+        // this.#client?.unsubscribeAsync("$SYS/#")
     }
 
     /**
      * 退出登录
      */
     logout() {
+        log.info('[mqtt][logout]');
+        this.#lastDisconnectReason = {
+            type: EDisconnectType.NORMAL
+        }
+        this.connectStatus = "DISCONNECTED"
         this.destoryClient()
     }
 
@@ -162,15 +281,15 @@ class ChatProtocol {
      * @returns 
      */
     async sendMsg(msg: IMQTTMsg) {
-        if (!this.client) {
+        if (!this.#client) {
             throw new Error("未登陆")
         }
 
         const topic = `${msgTopicPrefix}/${msg.chatType == IChatType.GROUP_CHAT ? "GROUP@" : ""}${msg.to}/${msg.from}`
 
-        console.log('[im][api][sendMsg] request', topic, msg);
-        const res = await this.client.publishAsync(topic, JSON.stringify(msg), { qos: 0 })
-        console.log('[im][api][sendMsg] response', res);
+        log.info('[mqtt][sendMsg] request', topic, msg);
+        const res = await this.#client.publishAsync(topic, JSON.stringify(msg), { qos: 1 })
+        log.info('[mqtt][sendMsg] response', res);
 
         return res
     }
@@ -180,9 +299,9 @@ class ChatProtocol {
      * @param topic 
      * @param message 
      */
-    onMessage(topic: any, message: any) {
+    private onMessage(topic: string, message: Uint8Array) {
         // message is Buffer
-        console.log(topic, message.toString(), 'onMessage');
+        this.#eventBus.emit(ETransportLayerEventName.MESSAGE_RECEIVED, topic, message)
     }
 
     /**
@@ -191,9 +310,33 @@ class ChatProtocol {
      * @param type 
      * @returns 
      */
-    async subscribeMsg(id: string, type: IChatType = IChatType.CHAT) {
+    private async subscribeMsg(id: string, type: IChatType = IChatType.CHAT) {
         const topic = `${msgTopicPrefix}/${type == IChatType.GROUP_CHAT ? "GROUP@" : ""}${id}/#`
-        return this.client?.subscribeAsync(topic)
+        log.info(`[mqtt][subscribeMsg] topic = ${topic}`);
+        return this.#client?.subscribeAsync(topic, { qos: 1 })
+    }
+
+    /**
+     * 监听事件
+     * @param name 
+     * @param handle 
+     * @returns Function
+     */
+    addEventListener<K extends keyof ITransportLayerEvent>(
+        name: K,
+        handle: ITransportLayerEvent[K]
+    ): Function {
+        return this.#eventBus.on(name, handle as any);
+    }
+
+
+    /**
+     * 销毁
+     */
+    destroy() {
+        log.debug('[mqtt][destroy]')
+        this.logout()
+        this.#eventBus.clear()
     }
 }
 
